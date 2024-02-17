9b67db35cf8df9945098ac73896ffe3d9b5af55b
Ananth Nag Kali <145688671+AnanthNagKali@users.noreply.github.com>
2024-02-17 19:48:54 +0530

    Add files via upload

---
+++ speech recognition demo.py
@@ -0,0 +1,24 @@
+import speech_recognition as sr
+def main():
+    r=sr.Recognizer()
+
+    #listen  
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source)
+        print("Please say something")
+        audio=r.listen(source)
+        print("Recognizing now")
+
+        #recognize with google
+        try:
+            print("You have said \n"+r.recognize_google(audio))
+            print("successfully recognized")
+        except Exception as e:
+            print("Error: "+str(e))
+        
+        #write
+        with open("recorded.wav","wb") as f:
+            f.write(audio.get_wav_data())
+
+if __name__=="__main__":
+    main()
